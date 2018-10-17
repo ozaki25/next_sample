848cc92f70bb93ebb8131b270720bb007a454779
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-18 07:21:19 +0900

    add NoSSR component

--- pages/todo.4.js
+++ pages/todo.4.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NoSSR from 'react-no-ssr';
 import { Provider } from 'react-redux';
 import store from '../src/modules/createStore';
 import TodoList from '../src/containers/TodoList';
 
 export default () => (
-  <Provider store={store}>
-    <TodoList />
-  </Provider>
+  <NoSSR>
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  </NoSSR>
 );
